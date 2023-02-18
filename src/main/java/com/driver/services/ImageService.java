01b57b9554ff5248c777ee698f6d146f5c39269b
Aman Sagar <109031753+Amansagarmithu@users.noreply.github.com>
2023-02-18 19:06:14 +0530

    Update ImageService.java

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -1,7 +1,8 @@
 package com.driver.services;
 
 import com.driver.models.*;
-import com.driver.repositories.*;
+import com.driver.repositories.BlogRepository;
+import com.driver.repositories.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,23 +10,71 @@ import java.util.List;
 
 @Service
 public class ImageService {
-
-    @Autowired
-    BlogRepository blogRepository2;
     @Autowired
     ImageRepository imageRepository2;
 
-    public Image addImage(Integer blogId, String description, String dimensions){
-        //add an image to the blog
+    @Autowired
+    BlogRepository blogRepository;
+
+    public Image createAndReturn(Blog blog, String description, String dimensions){
+        //create an image based on given parameters and add it to the imageList of given blog
+        Image image = new Image();
+        image.setDescription(description);
+        image.setDimensions(dimensions);
+        image.setBlog(blog);
+        imageRepository2.save(image);
+        return image;
+    }
+
+    public void deleteImage(Image image){
+
+        imageRepository2.delete(image);
+    }
+
+    public Image findById(int id) {
+        Image image = imageRepository2.findById(id).get();
+        return image;
 
     }
 
-    public void deleteImage(Integer id){
-
-    }
-
-    public int countImagesInScreen(Integer id, String screenDimensions) {
+    public int countImagesInScreen(Image image, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        //In case the image is null, return 0
+//        int ans = 0;
+//        String size = image.getDimensions();
+//        int sizeValue = size.charAt(0)*size.charAt(2);
+//        int screenDimensionValue = screenDimensions.charAt(0)*screenDimensions.charAt(2);
+//        ans = sizeValue/screenDimensionValue;
+//        return ans;
+        String dimensions = image.getDimensions();
+        int xi = 0;
+        int yi = 0;
+        int xs = 0;
+        int ys = 0;
+        int num = 0;
+        for(int i = 0; i<dimensions.length(); i++){
+            if(dimensions.charAt(i) == 'X'){
+                xi = num;
+                num = 0;
+                continue;
+            }
+            num *= 10;
+            num += (dimensions.charAt(i) - '0');
+        }
+        yi = num;
+        num = 0;
+        for(int i = 0; i<screenDimensions.length(); i++){
+            if(screenDimensions.charAt(i) == 'X'){
+                xs = num;
+                num = 0;
+                continue;
+            }
+            num *= 10;
+            num += (screenDimensions.charAt(i) - '0');
+        }
+        ys = num;
 
+        int ans = (int) (Math.floor((new Double(xs))/(new Double(xi))) * Math.floor((new Double(ys))/(new Double(yi))));
+        return ans;
     }
 }
